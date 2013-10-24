6c3df5f1ad86cd78394a62f56c4327756f4488b1
Aaron Jones <r.aaron.jones@gmail.com>
2013-10-24 15:08:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Long Lived FB Access Token
+========
+
+Long Lived Facebook Access Token Generator
+
+Simply follow the prompts and the resulting token will be valid for 60 days. 
+Great for use in the new Unity Facebook SDK!
---
+++ gettoken.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+
+echo "---------------------------------------------------------------------"
+echo "---     Welcome to the Long Lived Access Token exchange tool      ---"
+echo "--- all info can be found @ https://developers.facebook.com/apps/ ---"
+echo "---------------------------------------------------------------------"
+echo "Paste your Facebook application ID:  "
+read APPID
+
+echo "Paste your Facebook App Secret : "
+read APPSECRET
+
+echo "Paste your current short-lived access token : (https://developers.facebook.com/tools/access_token)"
+read ACCESSTOKEN
+
+echo "------------------------------------"
+echo "Using App ID [$APPID]" 
+echo "Using App Secret [$APPSECRET]" 
+echo "Using Access Token [$ACCESSTOKEN]" 
+echo "------------------------------------"
+
+echo "Retrieving long lived token ..."
+
+URL="https://graph.facebook.com/oauth/access_token?client_id=$APPID&grant_type=fb_exchange_token&client_secret=$APPSECRET&fb_exchange_token=$ACCESSTOKEN"
+echo "  URL: $URL"
+
+app_json=$(curl "$URL")
+echo " YOUR NEW LONG LIVED ACCESS TOKEN : $app_json"
